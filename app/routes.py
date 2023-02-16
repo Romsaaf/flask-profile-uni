83089db25bc5820400ebcf1edba883cf1522e96d
Romsaaf <romsaaf@mail.ru>
2023-02-16 22:11:46 +0300

    Swapped to one-card layout

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,5 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-   data_dict = {'name' : 'Roman', 'surname' : 'Safiullin', 'mail' : 'romsaaf@mail.ru'}
+   data_dict = {'name' : 'Roman', 'surname' : 'Safiullin', 'mail' : 'romsaaf@mail.ru', 'job':'Threat Intelligence engineer', 'linked':'https://www.linkedin.com/in/roman-safiullin-a09528252/', 'git':'https://github.com/Romsaaf', 'tg':'https://t.me/romsaaf'}
    return render_template('profile.html', data = data_dict)
